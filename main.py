2d2b0b654fac315d66d0a48d69e458d7658965cf
Sebastian <129107648+sebandrien@users.noreply.github.com>
2023-04-10 14:22:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import numpy as np
 import cv2
 
-image_path = "roompeople.jpg"
+image_path = "roompeople.jpg" #Define "image_path"
+prototxt_path = 
+model_path = 
 
+min_confidence = 0.25 #Using a minimum confidence value of 0.25
+
+classes = []
 
 np.random.seed(543210)
-colors = np.random
-min_confidence = 0.25
+colors = np.random.uniform(0,255, size=(len(classes),3))
 
-net = 
+net = cv2.dnn.readNetFromCaffe(prototxt_path, model_path) #Creating a neural network called net
+
 
 image = cv2.imread(image_path)
 height, weight = 
@@ -16,3 +21,22 @@ blobl =
 
 net.setInput(blob)
 detected_objects = net.foward()
+
+for i in range(detected_objects[0][0][i][
+  
+  if confidence > min_confidence:
+   class_index = int(detected_objects[0,0,i,1])
+  
+   upper_left_x = int(
+   upper_left_x = int(
+   upper_left_x = int(
+   upper_left_x = int(
+                                         
+                                         
+
+print(detected_objects[0][0][0][0])
+
+
+cv2.imshow("Detected objects", image)
+cv2.waitkey(0) #Continue executing on key press
+cv2.destroyAllWindows() #Close all windows
